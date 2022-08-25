0e100caec6c2cb28d3436051f8a459e3658e8ceb
Gonzalo <aguilarsimon01@gmail.com>
2022-08-25 09:08:40 -0300

    setting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Grid, MenuItem, TextField, Typography } from '@mui/material';
 
 function App() {
+  const [data, setData] = useState<Object>(Object)
+  const [location, setLocation] = useState<string>('cordoba')
+  const [city, setCity] = useState<string>('cordoba');
+  const URL: string = `https://api.openweathermap.org/data/3.0/onecall?lat={34.603722}&lon={58.381592}&appid=4f614993e61b575abedbf67c0b5a4514`
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid className="App">
+      <TextField
+        select
+        label="City"
+        value={city}
+        onChange={handleChange}
+        helperText="Please select a city"
+      >{cities.map((option) => (
+        <MenuItem key={option.value} value={option.value}>
+          {option.label}
+        </MenuItem>
+      ))}
+      </TextField>
+      <Typography variant='h1'>0C</Typography>
+      <Typography variant='h2'>Weather</Typography>
+    </Grid >
   );
 }
 
 export default App;
+
+
+const cities = [
+  {
+    value: 'santefe',
+    label: 'Santa Fe',
+  },
+  {
+    value: 'mendoza',
+    label: 'Mendoza',
+  },
+  {
+    value: 'buenosaires',
+    label: 'Buenos Aires',
+    lat: '34.603722',
+    lon: '58.381592',
+  },
+  {
+    value: 'cordoba',
+    label: 'Córdoba',
+  },
+];
